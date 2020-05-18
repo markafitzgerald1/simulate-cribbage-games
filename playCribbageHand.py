15681f0060b37cc2de6ed06ecf49fddc9c15b1c8
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-05-18 00:12:36 -0400

    Play fixed hands for about one second
    ...where one second is the time spent on my old computer (Intel Core
    i5-460M)

--- playCribbageHand.py
+++ playCribbageHand.py
@@ -1,7 +1,8 @@
-hands = [[0, 1, 2, 3], [4, 5, 6, 7]]
-playerToPlay = 0
-while len(hands[0]) + len(hands[1]) > 0:
-    if len(hands[playerToPlay]) > 0:
-        playerToPlayPlay = hands[playerToPlay].pop()
-        print(f"Player {playerToPlay} has a play: {playerToPlayPlay}.")
-    playerToPlay = (playerToPlay + 1) % 2
+for hand in range(115000):
+    hands = [[0, 1, 2, 3], [4, 5, 6, 7]]
+    playerToPlay = 0
+    while len(hands[0]) + len(hands[1]) > 0:
+        if len(hands[playerToPlay]) > 0:
+            playerToPlayPlay = hands[playerToPlay].pop()
+            # print(f"Player {playerToPlay} has a play: {playerToPlayPlay}.")
+        playerToPlay = (playerToPlay + 1) % 2
