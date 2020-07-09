b790260e33af4dccb49a1c0967f4f16b1131e7eb
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-07-08 23:10:13 -0400

    Output Node.js time per simulated hand

--- playCribbageHand.js
+++ playCribbageHand.js
@@ -1,19 +1,26 @@
-const randomJs = require('random-js');
+const randomJs = require("random-js");
 
 const mersenneTwisterEngine = randomJs.MersenneTwister19937.autoSeed();
 const deck = Array.from(Array(52).keys());
 
 const nHands = process.argv.length > 2 ? parseInt(process.argv[2]) : 750000;
+const startTimeNs = process.hrtime.bigint();
 console.log(`Simulating ${nHands} hands`);
-[...Array(nHands)].forEach(_ => {
-    const handCards = randomJs.sample(mersenneTwisterEngine, deck, 8);
-    const hands = [handCards.slice(0, 4), handCards.slice(4)];
-    let playerToPlay = 0;
-    while (hands[0].length + hands[1].length > 0) {
-        if (hands[playerToPlay].length > 0) {
-            const playerToPlayPlay = hands[playerToPlay].pop();
-            // console.log(`Player ${playerToPlay} has a play: ${playerToPlayPlay}.`);
-        }
-        playerToPlay = (playerToPlay + 1) % 2;
+[...Array(nHands)].forEach((_) => {
+  const handCards = randomJs.sample(mersenneTwisterEngine, deck, 8);
+  const hands = [handCards.slice(0, 4), handCards.slice(4)];
+  let playerToPlay = 0;
+  while (hands[0].length + hands[1].length > 0) {
+    if (hands[playerToPlay].length > 0) {
+      const playerToPlayPlay = hands[playerToPlay].pop();
+      // console.log(`Player ${playerToPlay} has a play: ${playerToPlayPlay}.`);
     }
+    playerToPlay = (playerToPlay + 1) % 2;
+  }
 });
+const elapsedTimeNs = process.hrtime.bigint() - startTimeNs;
+console.log(
+  `Simulated ${nHands} hands in ${elapsedTimeNs} ns for ${
+    elapsedTimeNs / BigInt(nHands)
+  } ns per hand`
+);
