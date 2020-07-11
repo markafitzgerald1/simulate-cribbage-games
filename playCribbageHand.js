6bf64e639e10145290b1dd9a474b4eef0722aa63
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-07-10 20:47:53 -0400

    Add MPL 2 License Headers

--- playCribbageHand.js
+++ playCribbageHand.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 const randomJs = require("random-js");
 
 const mersenneTwisterEngine = randomJs.MersenneTwister19937.autoSeed();
