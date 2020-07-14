0953c95ae8f76909a1ae005a0a0ec9f96f9f20a7
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-07-14 00:09:43 -0400

    2x speed up Java hands deal

--- src/main/java/PlayCribbageHand.java
+++ src/main/java/PlayCribbageHand.java
@@ -3,6 +3,9 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,10 +16,13 @@ public class PlayCribbageHand {
         final var deck = IntStream.range(0, 52).boxed().collect(Collectors.toCollection(ArrayList::new));
         final var nHands = argv.length >= 1 ? Integer.parseInt(argv[0]) : 1000;
         final var randomDataGenerator = new RandomDataGenerator();
+        final var random = new Random();
 
         final var startTimeNs = System.nanoTime();
         IntStream.range(0, nHands).forEach((number) -> {
-            randomDataGenerator.nextSample(deck, 8);
+            var handsCards = dealTwoHands(random);
+            // System.out.println(String.format("Deal: %s", Arrays.toString(handsCards)));
+
             // TODO: split into two hands
 
             // TODO: set player to play to 0
@@ -28,5 +34,13 @@ public class PlayCribbageHand {
         final var elapsedTimeNs = System.nanoTime() - startTimeNs;
         System.out.println(String.format("Simulated %d hands in %d ns for %f ns per hand", nHands, elapsedTimeNs,
                 ((double) elapsedTimeNs) / nHands));
+    }
+
+    private static Integer[] dealTwoHands(final Random random) {
+        var deal = new LinkedHashSet<Integer>();
+        do {
+            deal.add(random.nextInt(52));
+        } while (deal.size() < 8);
+        return (Integer[]) deal.toArray(new Integer[8]);
     }
 }
