89cbe43455207090de83320d989e6e52ae732f0e
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-08-01 00:05:44 -0400

    Use worker threads in Node.js implementation
    Node.js over 2x faster on my 2-core, 4-hyperthread CPU.

--- playCribbageHand.js
+++ playCribbageHand.js
@@ -1,31 +1,33 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-const randomJs = require("random-js");
-
-const mersenneTwisterEngine = randomJs.MersenneTwister19937.autoSeed();
-const deck = Array.from(Array(52).keys());
+const { Worker } = require("worker_threads");
+const os = require("os");
 
 const nHands = process.argv.length > 2 ? parseInt(process.argv[2]) : 730000;
-const startTimeNs = process.hrtime.bigint();
-console.log(`Simulating ${nHands} hands`);
-[...Array(nHands)].forEach((_) => {
-  const handCards = randomJs.sample(mersenneTwisterEngine, deck, 8);
-  const hands = [handCards.slice(0, 4), handCards.slice(4)];
-  // console.log(`Deal: ${hands}.`);
-  let playerToPlay = 0;
-  while (hands[0].length + hands[1].length > 0) {
-    if (hands[playerToPlay].length > 0) {
-      const playerToPlayPlay = hands[playerToPlay].pop();
-      // console.log(`Player ${playerToPlay} has a play: ${playerToPlayPlay}.`);
-    }
-    playerToPlay = (playerToPlay + 1) % 2;
-  }
-});
-const elapsedTimeNs = process.hrtime.bigint() - startTimeNs;
-console.log(
-  `Simulated ${nHands} hands in ${elapsedTimeNs} ns for ${
-    elapsedTimeNs / BigInt(nHands)
-  } ns per hand`
-);
+const nWorkers =
+  process.argv.length > 3 ? parseInt(process.argv[3]) : os.cpus().length;
+
+if (nWorkers === 1) {
+  require("./playCribbageHandWorker");
+} else {
+  console.log(`Simulating ${nHands} hands across ${nWorkers} worker threads`);
+  const startTimeNs = process.hrtime.bigint();
+  let nWorkersDone = 0;
+  const workers = Array.from(Array(nWorkers).keys()).map(
+    (_) =>
+      new Worker("./playCribbageHandWorker.js", {
+        argv: [Math.floor(nHands / nWorkers)],
+      })
+  );
+  workers.forEach((worker) =>
+    worker.on("exit", (code) => {
+      nWorkersDone++;
+      if (nWorkersDone === workers.length) {
+        const elapsedTimeNs = process.hrtime.bigint() - startTimeNs;
+        console.log(
+          `Simulated ${nHands} total hands in ${elapsedTimeNs} ns for ${
+            elapsedTimeNs / BigInt(nHands)
+          } ns per hand`
+        );
+      }
+    })
+  );
+}
---
+++ playCribbageHandWorker.js
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+const randomJs = require("random-js");
+
+const mersenneTwisterEngine = randomJs.MersenneTwister19937.autoSeed();
+const deck = Array.from(Array(52).keys());
+
+const nHands = process.argv.length > 2 ? parseInt(process.argv[2]) : 730000;
+// console.log(`Worker simulating ${nHands} hands`);
+const startTimeNs = process.hrtime.bigint();
+[...Array(nHands)].forEach((_) => {
+  const handCards = randomJs.sample(mersenneTwisterEngine, deck, 8);
+  const hands = [handCards.slice(0, 4), handCards.slice(4)];
+  // console.log(`Deal: ${hands}.`);
+  let playerToPlay = 0;
+  while (hands[0].length + hands[1].length > 0) {
+    if (hands[playerToPlay].length > 0) {
+      const playerToPlayPlay = hands[playerToPlay].pop();
+      // console.log(`Player ${playerToPlay} has a play: ${playerToPlayPlay}.`);
+    }
+    playerToPlay = (playerToPlay + 1) % 2;
+  }
+});
+const elapsedTimeNs = process.hrtime.bigint() - startTimeNs;
+console.log(
+  `Worker simulated ${nHands} hands in ${elapsedTimeNs} ns for ${
+    elapsedTimeNs / BigInt(nHands)
+  } ns per hand`
+);
