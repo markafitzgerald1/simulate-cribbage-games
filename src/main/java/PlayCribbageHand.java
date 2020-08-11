51a4e1ccf8f9e5401730df6c74e7f2a3f55853f3
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-08-10 23:38:59 -0400

    Add play count to Java code

--- src/main/java/PlayCribbageHand.java
+++ src/main/java/PlayCribbageHand.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PlayCribbageHand {
+    @SuppressWarnings("unchecked")
     public static void main(String[] argv) {
         final var nHands = argv.length >= 1 ? Integer.parseInt(argv[0]) : 1700000;
         final var random = new Random();
@@ -16,18 +18,25 @@ public class PlayCribbageHand {
         final var startTimeNs = System.nanoTime();
         IntStream.range(0, nHands).parallel().forEach((number) -> {
             final var handsCards = dealTwoHands(random);
-            // System.out.println(String.format("Deal: %s", Arrays.toString(handsCards)));
+            System.out.println(String.format("Deal: %s",
+                    Arrays.stream(handsCards).map(card -> getCardString(card)).collect(Collectors.toList())));
 
             final var handsCardsList = Arrays.asList(handsCards);
             final var hands = new ArrayList[] { new ArrayList<Integer>(handsCardsList.subList(0, 4)),
                     new ArrayList<Integer>(handsCardsList.subList(4, 8)) };
+            System.out.println(String.format("Hands: %s",
+                    Arrays.stream(hands).map(hand -> ((ArrayList<Integer>) hand).stream()
+                            .map(card -> getCardString(card)).collect(Collectors.toList()))
+                            .collect(Collectors.toList())));
 
             var playerToPlay = 0;
+            var playCount = 0;
             while (hands[0].size() + hands[1].size() > 0) {
                 if (hands[playerToPlay].size() > 0) {
-                    /* final var playerToPlayPlay = */ hands[playerToPlay].remove(hands[playerToPlay].size() - 1);
-                    // System.out.println(String.format("Player %d has a play: %s", (playerToPlay +
-                    // 1), playerToPlayPlay));
+                    final var playerToPlayPlay = (int) (hands[playerToPlay].remove(hands[playerToPlay].size() - 1));
+                    playCount += getPlayCount(playerToPlayPlay);
+                    System.out.println(String.format("Player %d plays %s for %d", (playerToPlay + 1),
+                            getCardString(playerToPlayPlay), playCount));
                 }
                 playerToPlay = (playerToPlay + 1) % 2;
             }
@@ -44,5 +53,13 @@ public class PlayCribbageHand {
             deal.add(random.nextInt(52));
         } while (deal.size() < 8);
         return (Integer[]) deal.toArray(new Integer[8]);
+    }
+
+    private static int getPlayCount(int playerToPlayPlay) {
+        return Math.min((playerToPlayPlay % 13) + 1, 10);
+    }
+
+    private static String getCardString(int card) {
+        return String.format("%c%c", "A23456789TJQK".charAt(card % 13), "♣♦♥♠".charAt(card / 13));
     }
 }
