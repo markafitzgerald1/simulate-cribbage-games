d9eab91a37a1873d1105c5483e2fab010474cd84
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-08-11 00:09:21 -0400

    Add 31 count limit to Java code

--- src/main/java/PlayCribbageHand.java
+++ src/main/java/PlayCribbageHand.java
@@ -16,28 +16,46 @@ public class PlayCribbageHand {
         final var random = new Random();
 
         final var startTimeNs = System.nanoTime();
-        IntStream.range(0, nHands).parallel().forEach((number) -> {
+        IntStream.range(0, nHands).forEach((number) -> {
             final var handsCards = dealTwoHands(random);
-            System.out.println(String.format("Deal: %s",
-                    Arrays.stream(handsCards).map(card -> getCardString(card)).collect(Collectors.toList())));
+            // System.out.println(String.format("Deal: %s",
+            // Arrays.stream(handsCards).map(card ->
+            // getCardString(card)).collect(Collectors.toList())));
 
             final var handsCardsList = Arrays.asList(handsCards);
             final var hands = new ArrayList[] { new ArrayList<Integer>(handsCardsList.subList(0, 4)),
                     new ArrayList<Integer>(handsCardsList.subList(4, 8)) };
-            System.out.println(String.format("Hands: %s",
-                    Arrays.stream(hands).map(hand -> ((ArrayList<Integer>) hand).stream()
-                            .map(card -> getCardString(card)).collect(Collectors.toList()))
-                            .collect(Collectors.toList())));
+            // System.out.println(String.format("Hands: %s",
+            // Arrays.stream(hands).map(hand -> ((ArrayList<Integer>) hand).stream()
+            // .map(card -> getCardString(card)).collect(Collectors.toList()))
+            // .collect(Collectors.toList())));
 
             var playerToPlay = 0;
             var playCount = 0;
+            var consecutiveGoCount = 0;
             while (hands[0].size() + hands[1].size() > 0) {
-                if (hands[playerToPlay].size() > 0) {
-                    final var playerToPlayPlay = (int) (hands[playerToPlay].remove(hands[playerToPlay].size() - 1));
+                final var currentPlayCount = playCount;
+                final var playableCard = ((ArrayList<Integer>) hands[playerToPlay]).stream()
+                        .filter(card -> currentPlayCount + getPlayCount((int) card) <= 31).findFirst();
+                if (playableCard.isPresent()) {
+                    final var playerToPlayPlay = playableCard.get();
+                    hands[playerToPlay].remove(playerToPlayPlay);
                     playCount += getPlayCount(playerToPlayPlay);
-                    System.out.println(String.format("Player %d plays %s for %d", (playerToPlay + 1),
-                            getCardString(playerToPlayPlay), playCount));
+                    // System.out.println(String.format("Player %d plays %s for %d", (playerToPlay +
+                    // 1),
+                    // getCardString(playerToPlayPlay), playCount));
+                    consecutiveGoCount = 0;
+                } else {
+                    // System.out.println(String.format("Player %d says \"Go!\"", (playerToPlay +
+                    // 1)));
+                    consecutiveGoCount++;
+                    if (consecutiveGoCount == 2) {
+                        // System.out.println("Resetting play count to 0.");
+                        consecutiveGoCount = 0;
+                        playCount = 0;
+                    }
                 }
+
                 playerToPlay = (playerToPlay + 1) % 2;
             }
         });
