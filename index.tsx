42596574395fe2ba35c76eb66759387d4928d74c
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2021-04-01 14:10:02 -0400

    Add do-nothing Deal button to web UI

--- index.tsx
+++ index.tsx
@@ -4,4 +4,14 @@ import ReactDOM from "react-dom";
 
 console.log("Hello from tsx!");
 
-ReactDOM.render(<h1>Play Cribbage</h1>, document.getElementById("root"));
+function DealCardsButton() {
+  return <button type="button">Deal</button>;
+}
+
+ReactDOM.render(
+  <div>
+    <h1>Play Cribbage</h1>
+    <DealCardsButton />
+  </div>,
+  document.getElementById("root")
+);
