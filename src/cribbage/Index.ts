61ea7eac31a1633033ceb4d79d36359c329d41ed
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2021-04-20 22:55:23 -0400

    Add missing MPL 2.0 license headers

--- src/cribbage/Index.ts
+++ src/cribbage/Index.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 export default class Index {
   static readonly TOTAL_COUNT: number = 13;
   static readonly STRINGS: Array<string> = "A23456789TJQK".split("");
