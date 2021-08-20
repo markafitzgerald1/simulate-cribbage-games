d30de974f6dd4f7ba686b92f74e8496cd106729b
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2021-08-20 00:39:48 -0400

    Add first few Python simulator unit tests

---
+++ TestSimulateCribbageGames.py
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import unittest
+import simulateCribbageGames
+
+
+class TestSimulateCribbageGames(unittest.TestCase):
+    def test_index_count_ace(self):
+        self.assertEqual(simulateCribbageGames.index_count(0), 1)
+
+    def test_index_count_ten(self):
+        self.assertEqual(simulateCribbageGames.index_count(9), 10)
+
+    def test_index_count_jack(self):
+        self.assertEqual(simulateCribbageGames.index_count(10), 10)
+
+    def test_index_count_king(self):
+        self.assertEqual(simulateCribbageGames.index_count(12), 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
